bb5a37101e98157c03193cdbb31e96ed143a7541
custom_linux <customlinuxqa@gmail.com>
2022-12-30 13:11:03 +0300

    Переходы между страницами, возращаем Page Object

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,19 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-#        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-#        login_link.click()
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        # login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        # login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
+        # assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+
+def should_be_login_link(self):
+    assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
