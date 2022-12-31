a8b8c4c209201d7d0f6945cf44865a7e2a06d563
custom_linux <customlinuxqa@gmail.com>
2022-12-31 11:53:21 +0300

    используем 2 книги с промо и без (4 теста)

---
+++ pages/test_link.py
@@ -0,0 +1,12 @@
+from pages.url_rout import ProductPageUrl
+
+
+class ProductTestLink():
+    TEST_LINK = [
+        (ProductPageUrl.THE_SHELLCODERS_HANDBOOK_LINK),
+        (ProductPageUrl.THE_SHELLCODERS_HANDBOOK_PROMO_LINK),
+        (ProductPageUrl.CODERS_AT_WORK_LINK),
+        (ProductPageUrl.CODERS_AT_WORK_PROMO_LINK)
+    ]
+
+
