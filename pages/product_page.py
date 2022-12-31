ca7f4c381ec2569ab11b34d1b1779d90db866d0d
custom_linux <customlinuxqa@gmail.com>
2022-12-31 11:50:40 +0300

    метод добавления товара в корзину и функция для подсчета формулы

---
+++ pages/product_page.py
@@ -0,0 +1,40 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+from .login_page import LoginPage
+from pages.url_rout import ProductPageUrl
+from selenium.common.exceptions import NoAlertPresentException
+
+import math
+
+
+class ProductPage(BasePage):
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def check_promo(self):
+        if (ProductPageUrl.PROMO_URL) in self.url:
+            print("\nAT! Promo detected..")
+            self.solve_quiz_and_get_code()
+        else:
+            print("\nAT! Test without promo ..")
+
+    def add_to_basket(self):
+        self.browser.find_element(*ProductPageLocators.ADD_BASKET).click()
+        self.check_promo()
+        item_name_in_notification = self.browser.find_element(*ProductPageLocators.ITEM_NAME_IN_NOTIF).text
+        item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+        assert item_name == item_name_in_notification, \
+            f"AT ERROR! Excepted - '{item_name}', result - {item_name_in_notification}"
