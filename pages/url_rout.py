2ae9c88482e14b02793f13875221575bd6a6b180
custom_linux <customlinuxqa@gmail.com>
2022-12-31 17:46:14 +0300

    изменила PROMO_URL

--- pages/url_rout.py
+++ pages/url_rout.py
@@ -1,6 +1,6 @@
 class ProductPageUrl():
     BASE_URL = "http://selenium1py.pythonanywhere.com/"
-    PROMO_URL = "?promo=newYear"
+    PROMO_URL = "?promo=newYear2019"
 
     THE_SHELLCODERS_HANDBOOK_ENDPOINT = "catalogue/the-shellcoders-handbook_209/"
     THE_SHELLCODERS_HANDBOOK_LINK = BASE_URL + THE_SHELLCODERS_HANDBOOK_ENDPOINT
