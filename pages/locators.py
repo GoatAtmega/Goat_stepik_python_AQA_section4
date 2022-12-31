3c9f2580d597ea92366378046b57f079feeecf1a
custom_linux <customlinuxqa@gmail.com>
2022-12-31 11:47:10 +0300

    add class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -7,6 +7,13 @@ class MainPageLocators():
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
+class ProductPageLocators():
+    ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ITEM_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    ITEM_NAME_IN_NOTIF = (By.CSS_SELECTOR, ".alert-success:nth-child(1) strong")
+
+
+
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
