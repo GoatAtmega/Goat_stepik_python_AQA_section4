19bda2f609137cebc9830996f82acab79637b4e0
custom_linux <customlinuxqa@gmail.com>
2023-01-01 14:04:47 +0300

    new mark

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,14 @@
+import time
 import pytest
 from pages.product_page import ProductPage
 from pages.base_page import BasePage
 from pages.test_link import ProductTestLink
+from pages.test_link import ProductBugTestLink
 
 
-@pytest.mark.parametrize("test_link", ProductTestLink.TEST_LINK)
+@pytest.mark.xfail
+@pytest.mark.parametrize("test_link", ProductBugTestLink.TEST_LINK)
 def test_guest_can_add_product_to_basket(browser, test_link):
     page = ProductPage(browser, test_link)
     page.open()
     page.add_to_basket()
-
