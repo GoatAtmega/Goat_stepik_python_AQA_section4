ae44fa292f3ca23e7a28f084e3e22385abec30cd
custom_linux <customlinuxqa@gmail.com>
2023-02-06 18:03:31 +0300

    combined everything into a class

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -7,23 +7,24 @@ from pages.test_link import ProductBugTestLink
 import pytest
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-#    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    login_page = page.go_to_login_page()
-    login_page.should_be_login_page()
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-
+@pytest.mark.login_guest
 @pytest.mark.parametrize("test_link", ProductTestLink.TEST_LINK)
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, test_link):
-    page = MainPage(browser, test_link)
-    page.open()
-    page.go_to_view_basket()
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                      # открываем страницу
+    #    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+        login_page = page.go_to_login_page()
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser, test_link):
+        page = MainPage(browser, test_link)
+        page.open()
+        page.go_to_view_basket()
